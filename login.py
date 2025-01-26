8cf1c9ec79caebb431f6f1251b263536c4d032a5
Elisa Bibb <elisabibb@Elisas-MacBook-Pro-2.local>
2025-01-25 21:35:34 -0600

    testing a change

--- login.py
+++ login.py
@@ -10,7 +10,7 @@ client = MongoClient(uri, server_api=ServerApi('1'))
 # Send a ping to confirm a successful connection
 try:
     client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
+    print("Pinged your deployment. You successfully connected to MongoDB! haha")
 
 
 except Exception as e:
